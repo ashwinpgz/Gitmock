a4c4e0dd845636bee7a87697a6649cff663a5f1d
ashwinpgz <ashwinraj1992@gmail.com>
2019-11-25 00:22:00 +0530

    deleted

--- src/org/recall/Gitrecall.java
+++ src/org/recall/Gitrecall.java
@@ -1,24 +1,7 @@
 package org.recall;
 
 public class Gitrecall {
-	private void ashwin() {
-		// TODO Auto-generated method stub
-		System.out.println("ashwin");
-	}
-
-	private void ashwin1() {
-		// TODO Auto-generated method stub
-		System.out.println("ashwin");
-
-	}
-
 
 	
-	public static void main(String[] args) {
-		Gitrecall g = new Gitrecall();
-
-		g.ashwin();
-		g.ashwin1();
-	}
-
+	
 }
