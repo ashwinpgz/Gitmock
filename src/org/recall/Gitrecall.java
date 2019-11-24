44d0a596694a96b92cbdfe26cfbe49addcb2032d
ashwinpgz <ashwinraj1992@gmail.com>
2019-11-24 23:55:32 +0530

    pull

--- src/org/recall/Gitrecall.java
+++ src/org/recall/Gitrecall.java
@@ -12,17 +12,13 @@ public class Gitrecall {
 
 	}
 
-	private void ashwin2() {
-		// TODO Auto-generated method stub
-		System.out.println("ashwin3");
-	}
 
+	
 	public static void main(String[] args) {
 		Gitrecall g = new Gitrecall();
 
 		g.ashwin();
 		g.ashwin1();
-		g.ashwin2();
 	}
 
 }
